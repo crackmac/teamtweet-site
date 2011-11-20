b309b1e1074233f52f8d23c859035e5ca2b5e756
Kevin Duane <crackmac@gmail.com>
2011-11-19 20:52:14 -0800

    added @anywhere functionality

--- html/index.php
+++ html/index.php
@@ -46,9 +46,9 @@
 	    <h1>TeamTweet.net</h1>
 	</header>
 	<div class="grid_4">
-	<p id="special">2011 MLB World Series <br />
+	<!-- <p id="special">2011 MLB World Series <br />
 		<a href="http://teamtweet.net/cardinals" target="_blank">Cardinals</a> vs <a href="http://teamtweet.net/rangers" target="_blank">Rangers</a>
-	</p>
+	</p> -->
 	</div>
     <nav class="grid_12">
       <?php include "navigation.html" ?>
@@ -175,6 +175,7 @@
 			</div>
 
 		<aside class="grid_3"> <!-- Sidebar -->
+			<div id="tbox"></div>
 			<h3>We're just getting started</h3>
 			<p>It takes time to find the right bunch of tweeters for our lists so please excuse any lack of experts. There is always the Raw Feed and Trash Talk to keep you entertained. Read more <a href="#">About Us</a> and how things work.
 			 
@@ -194,6 +195,18 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
   <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
 
+  <!-- @anywhere script to add twitter functionality-->
+  <script src="http://platform.twitter.com/anywhere.js?id=jXQnVjqOGh4kSGTILOew&v=1" type="text/javascript"></script>
+
+	<script type="text/javascript">
+	  twttr.anywhere(function (T) {
+	    T("#tbox").tweetBox({
+	      height: 100,
+	      width: 220,
+	      defaultContent: "#teamtweet http://teamtweet.net"
+	    });
+	  });
+	</script>
 
   <!-- scripts concatenated and minified via ant build script-->
   <script src="js/plugins.js"></script>
---
+++ html/js/mylibs/anywhere.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+ 
+
+  twttr.anywhere(function (T) {
+
+    T("#tbox").tweetBox();
+
+  });
+
+ 
+
+</script>
